930291ab1677b61d06b55b90b7eff86d5e0756c7
MBbaha <168421621+MBbaha@users.noreply.github.com>
2025-06-19 09:59:09 +0500

    Update hisobot.js2

--- src/hisobot.js
+++ src/hisobot.js
@@ -19,7 +19,7 @@ const HisobotDashboard = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get('https://risola-backend.onrender.com/users/getUsers')
+    axios.get('https://backend-rislola.onrender.com/users/getUsers')
       .then(res => setUsers(res.data.data))
       .catch(err => console.error("Xatolik:", err));
   }, []);
