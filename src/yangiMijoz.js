16f6e5ae202ec91db02466de33fd1fa3f1f4d01d
MBbaha <168421621+MBbaha@users.noreply.github.com>
2025-07-02 16:51:33 +0500

    Update yangiMijoz.js10

--- src/yangiMijoz.js
+++ src/yangiMijoz.js
@@ -34,8 +34,8 @@ function YangiMijoz() {
       fam: !fam,
       ism: !ism,
       location: !location,
-      sana: !sana || inputDate > today,
-      tel: tel.length !== 13 || !tel.startsWith('+998'),
+      sana: !sana ,
+      tel: !tel.length !== 9,
       summa: !summa,
       baho: !baho,
     };
@@ -43,10 +43,10 @@ function YangiMijoz() {
     setErrors(validationErrors);
 
     if (Object.values(validationErrors).some(Boolean)) {
-      if (!sana || inputDate > today) {
+      if (!sana) {
         setErrorMsg("❌ Sana xato! Kelajak sanani kiritmang.");
-      } else if (tel.length !== 13 || !tel.startsWith('+998')) {
-        setErrorMsg("❌ Telefon raqami to‘liq emas! +998 bilan boshlanishi va 13 ta belgidan iborat bo‘lishi kerak.");
+      } else if (!tel.length !== 9) {
+        setErrorMsg("❌ Telefon raqami to‘liq emas! 9 ta belgidan iborat bo‘lishi kerak.");
       } else if (!baho) {
         setErrorMsg("❌ Iltimos, mijozga baho bering!");
       } else {
